cd06c59976a7a919d03ce496021bc190200764d2
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-15 16:30:47 +0530

    [Lalit] Add: Given 3 Integers Find the Maximum Number

--- FindMaxNumberTest/FindMaxTest.cs
+++ FindMaxNumberTest/FindMaxTest.cs
@@ -10,20 +10,20 @@ namespace FindMaxNumberTest
         public void GivenMaxFirstNumber_WhenAnalysed_ShouldReturn_FirstMax()
         {
             //AAA- ARRANGE, ACT, ASSERT
-            int result = maxNum.FindMaxInteger(50, 15, 5);
-            Assert.AreEqual(50, result);
+            int result = maxNum.FindMaxInteger(20, 15, 5);
+            Assert.AreEqual(20, result);
         }
         [Test]
         public void GivenMaxSecondNumber_WhenAnalysed_ShouldReturn_SecondMax()
         {
-            int result = maxNum.FindMaxInteger(15, 50, 5);
-            Assert.AreEqual(50, result);
+            int result = maxNum.FindMaxInteger(15, 20, 5);
+            Assert.AreEqual(20, result);
         }
         [Test]
         public void GivenMaxThirdNumber_WhenAnalysed_ShouldReturn_ThirdMax()
         {
-            int result = maxNum.FindMaxInteger(15, 5, 50);
-            Assert.AreEqual(50, result);
+            int result = maxNum.FindMaxInteger(15, 5, 20);
+            Assert.AreEqual(20, result);
         }
     }
 }
